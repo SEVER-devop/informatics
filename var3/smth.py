59f4972fdaac42d314e79e22cfc03277a62b616e
severnayaluna <117164408+severnayaluna@users.noreply.github.com>
2025-01-16 21:02:00 +0300

    gbh

--- var3/smth.py
+++ var3/smth.py
@@ -11,29 +11,37 @@ def create_spiral_matrix(n):
     # Направления для спирали (право, вниз, лево, вверх)
     directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     x, y = n // 2, n // 2  # Начальная позиция (центр матрицы)
+
     current_direction = 0  # Начинаем с направления "право"
-    
-    # Заполняем матрицу по спирали
+    step_count = 1  # Количество шагов в текущем направлении
+    steps_done = 0  # Количество выполненных шагов в текущем "рейсе"
+
     for i in range(n * n):
-        matrix[x][y] = i  + 1  # Заполняем случайным числом от 1 до 100
+        matrix[x][y] = i + 1  # Заполняем случайным числом от 1 до 100
+
         # Рассчитываем следующую позицию
         next_x = x + directions[current_direction][0]
         next_y = y + directions[current_direction][1]
-        
+
+        # Переходим к следующему шагу
+        x, y = next_x, next_y
+        steps_done += 1
+
         # Проверяем, нужно ли менять направление
-        if (0 <= next_x < n) and (0 <= next_y < n) and (matrix[next_x][next_y] == 0):
-            x, y = next_x, next_y
-        else:
+        if steps_done == step_count:
             # Меняем направление
             current_direction = (current_direction + 1) % 4
-            x += directions[current_direction][0]
-            y += directions[current_direction][1]
+            steps_done = 0
+
+            # Увеличиваем количество шагов каждые два направления
+            if current_direction % 2 == 0:
+                step_count += 1
 
     return matrix
 
-n = 6
+n = 5
 spiral_matrix = create_spiral_matrix(n)
 
 # Выводим матрицу
-for i in spiral_matrix:
-    print(i)
+for row in spiral_matrix:
+    print(' '.join(map(str, row)))
