7272099f216db9eca03d73b0f0df167cd3c0ad8c
severnayaluna <versh1ninsergey@yandex.ru>
2025-03-30 23:49:49 +0300

    Excel + matlab

--- 2sem/LR01/LR1.m
+++ 2sem/LR01/LR1.m
@@ -1,20 +1,20 @@
-﻿clc
+clc
 clear
 
 % Построение графика
-x = -5:0.2:5;
+x = -1:0.2:1;
 y = m(x);
 
 plot(x, y)
 grid on
 xlabel('x')
 ylabel('F(x)')
-title('График уравнения y = 2*sin(x+pi()/3)-0,5*x^2+1')
+title('График уравнения y = exp(-2*x) -2*x + 1')
 
 % Поиск корней с помощью fsolve и fzero
-f =@(x) 2*sin(x+(pi()/3))-0.5*(x.^2)+1;
+f =@(x) exp(-2*x) -2*x + 1;
 
-x01 = fzero(f, [-2, 0]);
+x01 = fzero(f, [0, 1]);
 disp('X1 fzero = ')
 fprintf('%.3f\n',x01)
 
@@ -22,37 +22,28 @@ x02 = fsolve(f, 0);
 disp('X1 fsolve = ')
 fprintf('%.3f\n',x02)
 
-x03 = fzero(f, [0, 2]);
-disp('X2 fzero = ')
-fprintf('%.3f\n',x03)
-
-x04 = fsolve(f, 1);
-disp('X2 fsolve = ')
-fprintf('%.3f\n',x04)
 
 % Поиск корней методом дихотомии
-for i = 1:2
-    a = input('Введите начальный интервал: ');
-    b = input('Введите конечный интервал: ');
+a = input('Введите начальный интервал: ');
+b = input('Введите конечный интервал: ');
 
-    e = 0.001;
+e = 0.001;
 
-    if f(a) * f(b) > 0
-        fprintf('На заданном интервале нет корней.\n');
-    else
-        while (b-a)/2 > e
-            c = (a + b) / 2;
-            if f(c) == 0
-                break;
-            elseif f(c) * f(a) < 0
-                b = c;
-            else
-                a = c;
-            end
+if f(a) * f(b) > 0
+    fprintf('На заданном интервале нет корней.\n');
+else
+    while (b-a)/2 > e
+        c = (a + b) / 2;
+        if f(c) == 0
+            break;
+        elseif f(c) * f(a) < 0
+            b = c;
+        else
+            a = c;
         end
-    
-        x03= (a + b) / 2;
-        disp('X3 = ')
-        fprintf('%.3f\n',x03)
     end
+    
+    x03= (a + b) / 2;
+    disp('X3 = ')
+    fprintf('%.3f\n',x03)
 end
--- 2sem/LR01/m.m
+++ 2sem/LR01/m.m
@@ -1,3 +1,3 @@
-﻿function y = m(x)
-    y = 2*sin(x+(pi()/3))-0.5*(x.^2)+1;
+function y = m(x)
+    y = exp(-2*x) -2*x + 1;
 end
