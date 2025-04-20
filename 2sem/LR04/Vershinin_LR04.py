a41ba8e4512fec7d906208ea7a8e01ca65abbe36
severnayaluna <versh1ninsergey@yandex.ru>
2025-04-20 21:21:32 +0300

    new methods

--- 2sem/LR04/Vershinin_LR04.py
+++ 2sem/LR04/Vershinin_LR04.py
@@ -7,20 +7,23 @@ import numpy as np
 
 
 class RootFinder:
-    def __init__(self):
-        self.values = [None for _ in range(5)]
+    def __init__(self) -> None:
+        self.values = [None for _ in range(12)]
         self.widgets = []
         self.eps = 1e-3
         self.a = 0
         self.b = 1
+        self.x_values = [0, 0.13, 0.26, 0.39,0.52,0.65, 0.79, 0.92, 1.05, 1.18, 1.31, 1.44, 1.57]
+        self.y_values = [0, 0.004, 0.033, 0.104, 0.217, 0.358, 0.5, 0.608, 0.65, 0.604, 0.467, 0.254, 0]
 
 
     def __func(self, x) -> None:
         return math.log(x + math.sqrt(x*x + 8))
 
 
-    def __left_rectangle_integral(self, precision=1e-3) -> None:
+    def __left_rectangle_analit_integral(self) -> None:
         func = self.__func
+        precision = self.eps
         a = self.a
         b = self.b
         n = 1
@@ -39,9 +42,9 @@ class RootFinder:
             n *= 2
         self.values[0] = integral
 
-
-    def __mid_rectangle_integral(self, precision=1e-3) -> None:
+    def __mid_rectangle_analit_integral(self) -> None:
         func = self.__func
+        precision = self.eps
         a = self.a
         b = self.b
         n = 1
@@ -61,9 +64,9 @@ class RootFinder:
             n *= 2
         self.values[1] = integral
 
-
-    def __right_rectangle_integral(self, precision=1e-3) -> None:
+    def __right_rectangle_analit_integral(self) -> None:
         func = self.__func
+        precision = self.eps
         a = self.a
         b = self.b
         n = 1
@@ -83,9 +86,9 @@ class RootFinder:
             n *= 2
         self.values[2] = integral
 
-
-    def __trapezoidal_integral(self, precision=1e-3) -> None:
+    def __trapezoidal_analit_integral(self) -> None:
         func = self.__func
+        precision = self.eps
         a = self.a
         b = self.b
         n = 1
@@ -105,9 +108,9 @@ class RootFinder:
             n *= 2
         self.values[3] = integral
 
-
-    def __simpson_integral(self, precision=1e-3) -> None:
+    def __simpson_analit_integral(self) -> None:
         func = self.__func
+        precision = self.eps
         a = self.a
         b = self.b
         n = 1
@@ -130,13 +133,81 @@ class RootFinder:
             n *= 2
         self.values[4] = integral
 
+    def __analytical_integral(self) -> None:
+        integral = (-3 +2*math.sqrt(2) + math.log(4))
+        self.values[5] = integral
 
-    def get_roots(self):
-        self.__left_rectangle_integral()
-        self.__mid_rectangle_integral()
-        self.__right_rectangle_integral()
-        self.__trapezoidal_integral()
-        self.__simpson_integral()
+
+    def __left_rectangles_table_integral(self) -> None:
+        integral = 0.0
+        x = self.x_values
+        y = self.y_values
+        for i in range(len(x) - 1):
+            integral += y[i] * (x[i+1] - x[i])
+        self.values[6] = integral
+
+    def __right_rectangles_table_integral(self) -> None:
+        integral = 0.0
+        x = self.x_values
+        y = self.y_values
+        for i in range(len(x) - 1):
+            integral += y[i+1] * (x[i+1] - x[i])
+        self.values[7] = integral
+
+    def __mid_rectangles_table_integral(self) -> None:
+        integral = 0.0
+        x = self.x_values
+        y = self.y_values
+        for i in range(len(x) - 1):
+            mid_y = (y[i] + y[i+1]) / 2
+            integral += mid_y * (x[i+1] - x[i])
+        self.values[8] = integral
+
+    def __trapezoidal_table_integral(self) -> None:
+        integral = 0.0
+        x = self.x_values
+        y = self.y_values
+        for i in range(len(x) - 1):
+            integral += (y[i] + y[i+1]) * (x[i+1] - x[i]) / 2
+        self.values[9] = integral
+
+    def __simpson_table_integral(self) -> None:
+        x = self.x_values
+        y = self.y_values
+
+        if (len(x) - 1) % 2 != 0:
+            raise ValueError("Для метода Симпсона необходимо четное число интервалов")
+        
+        integral = y[0] + y[-1]
+        h = x[1] - x[0]
+        
+        for i in range(1, len(x)-1):
+            if i % 2 == 1:
+                integral += 4 * y[i]
+            else:
+                integral += 2 * y[i]
+        
+        integral *= h / 3
+        self.values[10] = integral
+
+    def __technical_spec_table_integral(self) -> None:
+        self.values[11] = 0.5
+
+
+    def get_roots(self) -> list:
+        self.__left_rectangle_analit_integral()
+        self.__mid_rectangle_analit_integral()
+        self.__right_rectangle_analit_integral()
+        self.__trapezoidal_analit_integral()
+        self.__simpson_analit_integral()
+        self.__analytical_integral()
+        
+        self.__left_rectangles_table_integral()
+        self.__right_rectangles_table_integral()
+        self.__mid_rectangles_table_integral()
+        self.__trapezoidal_table_integral()
+        self.__simpson_table_integral()
+        self.__technical_spec_table_integral()
 
         return self.values
 
@@ -152,69 +223,82 @@ def error(er='Ошибка'):
     er_win.geometry("350x150")
     Label(er_win, text=er, font="30", bg="red").pack()
 
-
-    
-def disp_info(ent_accur) -> None:
+ 
+def disp_info(ent_accur, left, right) -> None:
     try:
         accuracity = float(ent_accur.get())
+        left_ent = int(left.get())
+        right_ent = int(right.get())
     except ValueError:
         return error("Введите численные значениия\n в границах")
     roots.eps = accuracity
+    roots.a = left_ent
+    roots.b = right_ent
+    plot_graph(left_ent, right_ent)
+    
 
     values = roots.get_roots()
-    names_methods = ["Лев. треуг.", "Средн. треуг.", "Прав. треуг.", "Трапец.", "Симпсон"]
+    names_methods = ["Лев. треуг.", "Средн. треуг.", "Прав. треуг.", "Трапец.", "Симпсон", "Аналитика"]
     names_up = ["Методы вычисления", "RES"]
-    table_frame = Frame(master=win, bg="peachpuff")
-    table_frame.place(x=500, y=250)
-    roots.widgets.append(table_frame)
+    table_frame_first = Frame(master=win, bg="peachpuff")
+    table_frame_first.place(x=750, y=70)
+    table_frame_second = Frame(master=win, bg="peachpuff")
+    table_frame_second.place(x=1200, y=70)
+    names_frame = [table_frame_first, table_frame_second]
+    roots.widgets.extend(names_frame)
+    names_integral = ["Интеграл, заданный функцией","Таблично заданный интеграл"]
 
+    for j in range(len(names_frame)):
+        Label(names_frame[j], text=names_integral[j], bg="peachpuff", font="15").grid(row=0, column=0, columnspan=2, padx=20)
+        for i in range(len(names_up)):
+            cell = Label(names_frame[j], text=f"{names_up[i]}", bg="peachpuff", font="15" )
+            cell.grid(row=1, column=i, padx=20, pady=20)
 
-    for i in range(len(names_up)):
-        cell = Label(table_frame, text=f"{names_up[i]}", bg="peachpuff", font="15" )
-        cell.grid(row=0, column=i, padx=20)
-
-    for i in range(len(names_methods)):
-        cell = Label(table_frame, text=f"{names_methods[i]}", bg="peachpuff", font="15" )
-        cell.grid(row=i+1, column=0, padx=20, pady=20)
-        cell = Label(table_frame, text=f"{values[i]}", bg="peachpuff", font="15" )
-        cell.grid(row=i+1, column=1, padx=20)
+        for i in range(len(names_methods)):
+            cell = Label(names_frame[j], text=f"{names_methods[i]}", bg="peachpuff", font="15" )
+            cell.grid(row=i+2, column=0, padx=20, pady=20)
+            cell = Label(names_frame[j], text=f"{values[i + j*6]}", bg="peachpuff", font="15" )
+            cell.grid(row=i+2, column=1, padx=20)
 
 
 def disp_matrix(mat, x1, y1=385) -> None:
 
-    table_frame = Frame(master=win, bg="bisque2")
+    table_frame = Frame(master=win, bg="peachpuff")
     table_frame.place(x=x1, y=y1)
     for i in range(len(mat)):
         if type(mat[i]) == list:
             for j in range(len(mat[i])):
-                cell = Label(table_frame, text=f"{mat[i][j]}", bg="bisque2", font="15" )
+                cell = Label(table_frame, text=f"{mat[i][j]}", bg="peachpuff", font="15" )
                 cell.grid(row=i, column=j, padx=10, pady=10)
         else:
-            cell = Label(table_frame, text=f"{mat[i]}", bg="bisque2", font="15" )
+            cell = Label(table_frame, text=f"{mat[i]}", bg="peachpuff", font="15" )
             cell.grid(row=i, column=0, padx=10, pady=10)
 
 
 def plot_graph(ent_left, ent_right) -> None:
 
-    try:
-        left_border = float(ent_left.get())
-        right_border = float(ent_right.get())
-    except ValueError:
-        return error("Введите численные значениия\n в границах")
-    roots.left_border = left_border
-    roots.right_border = right_border
-
+    if ent_left is not(type(10)):
+        try:
+            left_border = float(ent_left.get())
+            right_border = float(ent_right.get())
+        except ValueError:
+            return error("Введите численные значениия\n в границах")
+        except:
+            pass
+        left_border = roots.a
+        right_border = roots.b 
+    else:
+        left_border = ent_left
+        right_border = ent_right
 
     x = np.linspace(left_border, right_border, 1000)
-    y = np.linspace(left_border, right_border, 1000)
-    X, Y = np.meshgrid(x, y)
-
-    graph_1 = np.log(X + np.sqrt(X*X + 8))
+    y = np.log(x + np.sqrt(x**2 + 8))
 
     fig = Figure(figsize=(4, 4), facecolor='peachpuff')
     ax = fig.add_subplot(facecolor='bisque')
+    
+    ax.plot(x, y, color='r', linestyle='solid', linewidth=2)
 
-    ax.contour(X, Y, graph_1, levels=[-1], colors='r', linestyles='solid')
 
     ax.axhline(y=0, color='gray', linestyle='-', linewidth=1)
     fig.patch.set_edgecolor('whitesmoke')
@@ -222,7 +306,7 @@ def plot_graph(ent_left, ent_right) -> None:
 
     canvas1 = FigureCanvasTkAgg(fig, master = win)
     canvas1.draw()
-    canvas1.get_tk_widget().place(x=0, y=300)
+    canvas1.get_tk_widget().place(x=-1, y=300)
     # roots.widgets.append(canvas1)
     
 
@@ -235,31 +319,47 @@ def tkinter_fun() -> None:
     win.config(bg="bisque")
     Label(text="Вершинин Сергей АТ-24-01\nВариант №3", font="30", bg="bisque").place(x=70, y=740)
 
+
     Canvas(bg="peachpuff", width=395, height=300).place(x=1, y=1)
-    Canvas(bg="bisque2", width=395, height=270).place(x=1, y=301)
+    Canvas(bg="peachpuff", width=280, height=610).place(x=397, y=1)
+
+
+    # Интеграл
+    fig = Figure(figsize=(4, 3), facecolor='peachpuff')
+    ax = fig.add_subplot(111)
+    ax.axis('off')
+    integral_text = r"$\int_{0}^{1} \log \left( x + \sqrt{x^2 + 8} \right) \, dx$"
+    ax.text(0.5, 0.5, integral_text, fontsize=20, 
+            ha='center', va='center', color='black')
+    canvas = FigureCanvasTkAgg(fig, master=win)
+    canvas.draw()
+    canvas.get_tk_widget().place(x=-10, y=50)
+
 
     # Границы
-    Label(text="Границы", font="15", bg="peachpuff").place(x=23, y=105)
-    ent_left_border = Entry(win, textvariable=StringVar(value=-5), width=5, justify=CENTER)
-    ent_left_border.place(x=25, y=135)
-    ent_right_border = Entry(win, textvariable=StringVar(value=5), width=5, justify=CENTER)
-    ent_right_border.place(x=65, y=135)
+    Label(text="Границы", font="15", bg="peachpuff").place(x=105, y=65)
+    ent_left_border = Entry(win, textvariable=StringVar(value=0), width=5, justify=CENTER)
+    ent_left_border.place(x=105, y=95)
+    ent_right_border = Entry(win, textvariable=StringVar(value=1), width=5, justify=CENTER)
+    ent_right_border.place(x=145, y=95)
 
 
     # Уравнение
-    # Label(text="Матрица A", font="15", bg="bisque2").place(x=60, y=335)
-    # Label(text="Матрица B", font="15", bg="bisque2").place(x=255, y=335)
-    # disp_matrix(roots.matrix_a, 20)
-    # disp_matrix(roots.matrix_b, 280)
+    Label(text="Вектор X", font="15", bg="peachpuff").place(x=435, y=10)
+    Label(text="Вектор F(X)", font="15", bg="peachpuff").place(x=565, y=10)
+    disp_matrix(roots.x_values, 440, 30)
+    disp_matrix(roots.y_values, 570, 30)
 
     
     # Точность
-    Label(text="Точность", font="15", bg="peachpuff").place(x=140, y=125)
+    Label(text="Точность", font="15", bg="peachpuff").place(x=220, y=65)
     ent_accuracy = Entry(win, textvariable=StringVar(value="1e-3"), width=9, justify=CENTER)
-    ent_accuracy.place(x=150, y=155)
+    ent_accuracy.place(x=230, y=95)
 
     # Расчет всех корней
-    Button(win, text="Рассчитать корни", font="15", command= lambda x=ent_accuracy: disp_info(x)).place(x=100, y=50)
+    Button(win, text="Рассчитать корни", font="15", command= lambda x=ent_accuracy,
+                                                                    y=ent_left_border,
+                                                                    z=ent_right_border: disp_info(x, y, z)).place(x=130, y=20)
     
     plot_graph(ent_left_border, ent_right_border)
 
